ebc7268c49969772b042d66beb53da934cea4931
LotosCoder <volodimirantonov44@gmail.com>
2024-07-10 21:14:56 +0300

    Update module.py

--- module.py
+++ module.py
@@ -1,14 +1,91 @@
-from .. import loader
-from asyncio import sleep
+# █ █ █ █▄▀ ▄▀█ █▀▄▀█ █▀█ █▀█ █ █
+# █▀█ █ █ █ █▀█ █ ▀ █ █▄█ █▀▄ █▄█
+
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# 👤 https://t.me/hikamoru
+
+# meta developer: @hikamorumods
+# meta banner: https://raw.githubusercontent.com/AmoreForever/assets/master/Funquotes.jpg
+__version__ = (1, 0, 0)
+
+from telethon.tl.types import Message
+
+from .. import loader, utils
 
 
 @loader.tds
-class rap(loader.Module):
-    strings = {"name": "hityka"}
+class InlineFunMod(loader.Module):
+    """Create Fun quotes"""
 
-    @loader.owner
-    async def rapcmd(self, message):
-        for _ in range(1):
-            for rap in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня💋"]:
-             await message.edit(rap)
-                await sleep(0.3)
+    strings = {
+        "name": "FunQuotes",
+        "where_text": "<emoji document_id='6041914500272098262'>🚫</emoji> <b>Provide a text to create sticker with</b>",
+        "processing": (
+            "<emoji document_id='6318766236746384900'>🕔</emoji> <b>Processing...</b>"
+        ),
+    }
+
+    strings_ru = {
+        "where_text": "<emoji document_id='6041914500272098262'>🚫</emoji> <b>Укажи текст для создания стикера</b>",
+        "processing": (
+            "<emoji document_id='6318766236746384900'>🕔</emoji> <b>Обработка...</b>"
+        ),
+    }
+
+    async def glaxcmd(self, message: Message):
+        """<text> - Create Google search quote"""
+        text = utils.get_args_raw(message)
+        if not text:
+            await message.edit(self.strings("where_text"))
+            return
+
+        await message.edit(self.strings("processing"))
+
+        try:
+            query = await self._client.inline_query("@googlaxbot", text)
+            await message.respond(file=query[0].document)
+        except Exception as e:
+            await utils.answer(message, str(e))
+            return
+
+        if message.out:
+            await message.delete()
+        
+    async def twitcmd(self, message: Message):
+        """<text> - Create Twitter message quote"""
+        text = utils.get_args_raw(message)
+        if not text:
+            await message.edit(self.strings("where_text"))
+            return
+
+        await message.edit(self.strings("processing"))
+
+        try:
+            query = await self._client.inline_query("@TwitterStatusBot", text)
+            await message.respond(file=query[0].document)
+        except Exception as e:
+            await utils.answer(message, str(e))
+            return
+
+        if message.out:
+            await message.delete()
+            
+    async def frogcmd(self, message: Message):
+        """<text> - Create Frog text quote"""
+        text = utils.get_args_raw(message)
+        if not text:
+            await message.edit(self.strings("where_text"))
+            return
+
+        await message.edit(self.strings("processing"))
+
+        try:
+            query = await self._client.inline_query("@honka_says_bot", text + ".")
+            await message.respond(file=query[0].document)
+        except Exception as e:
+            await utils.answer(message, str(e))
+            return
+
+        if message.out:
+            await message.delete()         
